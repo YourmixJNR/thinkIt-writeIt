60568ab6c2d3e301bdad98e0463a76a78a76aeaa
YourmixJNR <radmakson@gmail.com>
2024-03-20 19:53:47 +0100

    updated

--- client/components/CustomInput.jsx
+++ client/components/CustomInput.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import {
   FormControl,
   FormLabel,
-  FormErrorMessage,
   Input,
   FormHelperText,
+  FormErrorMessage,
 } from "@chakra-ui/react";
+import PropTypes from "prop-types";
 
-const CustomInput = ({label}) => {
+const CustomInput = ({
+  label,
+  type,
+  isRequired,
+  isInvalid,
+  value,
+  onChange,
+  helperValue,
+  errorValue,
+}) => {
   return (
-    <FormControl>
+    <FormControl isRequired={isRequired} isInvalid={isInvalid}>
       <FormLabel>{label}</FormLabel>
-      <Input type="email" />
-      <FormHelperText>We'll never share your email.</FormHelperText>
+      <Input value={value} type={type} onChange={onChange} />
+      <FormHelperText>{helperValue}</FormHelperText>
+      <FormErrorMessage>{errorValue}</FormErrorMessage>
     </FormControl>
   );
+};
+
+CustomInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  isRequired: PropTypes.bool,
+  isRequired: PropTypes.any,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  helperValue: PropTypes.node,
+  errorValue: PropTypes.node,
 };
 
 export default CustomInput;
