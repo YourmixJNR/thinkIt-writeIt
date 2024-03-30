9a0c3777e10076befed647e01a49c7612a7f9a67
YourmixJNR <radmakson@gmail.com>
2024-03-30 04:48:02 +0100

    updated

--- client/components/navigation/Nav.jsx
+++ client/components/navigation/Nav.jsx
@@ -1,28 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import links from "./links";
 import NextLink from "next/link";
 import { Box, Link } from "@chakra-ui/react";
+import { AuthContext } from "../../context/auth/authContext";
 
 const Nav = ({ customStyles }) => {
+  const { state } = useContext(AuthContext);
+
   return (
     <Box as="nav" style={customStyles}>
       {links.map(({ name, icon, path }, index) => {
-        return (
-          <Link
-            as={NextLink}
-            href={path}
-            key={index}
-            display={"flex"}
-            alignItems={"center"}
-            gap={2}
-            _hover={{
-              textDecoration: "none",
-            }}
-          >
-            <span>{icon}</span>
-            <span>{name}</span>
-          </Link>
-        );
+        const isLogin = name === "Login";
+        const isLogout = name === "Logout";
+        const showLink = (isLogin && !state.user) || (isLogout && state.user);
+
+        if (showLink || !(isLogin || isLogout)) {
+          return (
+            <Link
+              as={NextLink}
+              href={path}
+              key={index}
+              display={"flex"}
+              alignItems={"center"}
+              gap={2}
+              _hover={{
+                textDecoration: "none",
+              }}
+            >
+              <span>{icon}</span>
+              <span>{name}</span>
+            </Link>
+          );
+        }
+
+        return null;
       })}
     </Box>
   );
--- client/components/navigation/links.js
+++ client/components/navigation/links.js
@@ -1,11 +1,13 @@
 import { TbLayoutGridAdd } from "react-icons/tb";
 import { TbUsersPlus } from "react-icons/tb";
 import { RiLoginBoxLine } from "react-icons/ri";
+import { RiLogoutBoxLine } from "react-icons/ri";
 
 const links = [
     { name: "App", icon: <TbLayoutGridAdd />, path: "/" },
     { name: "Sign Up", icon: <TbUsersPlus />, path: "/register" },
     { name: "Login", icon: <RiLoginBoxLine />, path: "/login" },
+    { name: "Logout", icon: <RiLogoutBoxLine />, path: "/login" },
 ]
 
 export default links
