9fd62c9e430260b90f50550a6a6df7df7a25cd82
Michael Victor <radmakson@gmail.com>
2024-04-07 23:04:45 +0100

    Update ProtectedRoute.jsx

--- client/utils/ProtectedRoute.jsx
+++ client/utils/ProtectedRoute.jsx
@@ -1,1 +1,18 @@
+import React, { useContext, useEffect } from "react";
+import { useRouter } from "next/router";
+import { AuthContext } from "../context/auth/authContext";
 
+const ProtectedRoute = ({ children }) => {
+ const { state } = useContext(AuthContext);
+ const router = useRouter();
+
+ useEffect(() => {
+    if (!state.isLoggedIn) {
+      router.push("/login");
+    }
+ }, [state.isLoggedIn, router]);
+
+ return children;
+};
+
+export default ProtectedRoute;
