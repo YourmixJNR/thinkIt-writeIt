ae2334cf602b6ff21412fda1774e10c344822948
YourmixJNR <radmakson@gmail.com>
2024-04-14 16:18:37 +0100

    updated

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -24,3 +24,38 @@ export const currentUser = async (req, res) => {
     });
   }
 };
+
+export const updateUser = async (req, res) => {
+  try {
+    const id = req.auth._id;
+    const { bio } = req.body;
+    if (!{ id }) {
+      return res.status(401).json({
+        message: "Unauthorized",
+        error: "No user token provided",
+      });
+    }
+    if (!bio) {
+      return res.status(401).json({
+        message: "Bad request",
+        error: "Bio required",
+      });
+    }
+    await User.findOneAndUpdate(
+      { id },
+      { bio },
+      {
+        new: true,
+      }
+    );
+    res.status(200).json({
+      message: "Profile updated",
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      message: "Internal server error",
+      error: err.message,
+    });
+  }
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { requireSignIn } from "../middlewares/authMiddleware.js";
-import { currentUser } from "../controllers/user.js";
+import { currentUser, updateUser } from "../controllers/user.js";
 
 const userRouter = Router();
 
 userRouter.get("/current-user", requireSignIn, currentUser);
+userRouter.post("/update-user", requireSignIn, updateUser);
 
 export default userRouter;
