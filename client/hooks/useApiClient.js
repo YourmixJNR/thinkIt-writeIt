8bfe203acf5d4f3cd9ca3084e267e1859dc0d7a7
YourmixJNR <radmakson@gmail.com>
2024-04-29 03:48:32 +0100

    updated

--- client/hooks/useApiClient.js
+++ client/hooks/useApiClient.js
@@ -24,14 +24,6 @@ export const useApiClient = () => {
         }
     });
 
-    apiClient.interceptors.request.use(function (config) {
-        // Do something before request is sent
-        return config;
-      }, function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-      });
-
     apiClient.interceptors.response.use(function (response) {
         // Do something with response data
         return response;
