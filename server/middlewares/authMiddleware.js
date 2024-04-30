c438551646dd42a8d9daa7be442fe03293b33553
Michael Victor <radmakson@gmail.com>
2024-04-30 23:35:21 +0100

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,7 +1,6 @@
 import { expressjwt } from "express-jwt";
 
 export const requireSignIn = expressjwt({
-  getToken: (req, res) => req.cookies.token,
   secret: () => process.env.JWT_SECRET,
   algorithms: ["HS256"],
 });
