67d7cc12e5f726f0db1d1755a86b6ed11f29535b
YourmixJNR <radmakson@gmail.com>
2024-05-06 17:26:20 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ import app from "./server.js";
 import startDb from "./config/db.js";
 import { processEnv } from "./lib/processEnv.js";
 
-// db
+// connect to MongoDB
 startDb();
 
 // port
 const port = `${processEnv.PORT}` || 8000;
 
+// start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
