c64256fdc1a98efa455aef2262c4be6446bf441b
YourmixJNR <radmakson@gmail.com>
2024-05-06 17:11:54 +0100

    updated

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
-import { app } from "./server";
-app()
+import app from "./server.js";
+import startDb from "./config/db.js";
+import { processEnv } from "./lib/processEnv.js";
+
+// db
+startDb();
+
+// port
+const port = `${processEnv.PORT}` || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
