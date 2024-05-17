f7a882b0620f185f55bb5420961e0bfecbfa91a0
YourmixJNR <radmakson@gmail.com>
2024-05-17 22:37:12 +0100

    init

--- client/components/constant/image.js
+++
@@ -1,7 +0,0 @@
-import SiteLogo from "../../public/SiteLogo.png"
-import HomeImage from "../../public/HomeImage.png"
-
-export default {
-    SiteLogo,
-    HomeImage
-}
--- client/env/loadEnv.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL
-
-export const loadEnv = {
-    API_URL
-}
